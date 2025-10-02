d24b72f1a3538644ea210ce8110a91d3a063eb9e
Carlos-Cao <carlos.cao2@gmail.com>
2025-10-02 20:45:30 +1300

    feat: add service interface and implementation

--- src/main/java/com/jobtracker/jobtrackerapi/JobTrackerApiApplication.java
+++ src/main/java/com/jobtracker/jobtrackerapi/JobTrackerApiApplication.java
@@ -9,5 +9,4 @@ public class JobTrackerApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobTrackerApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/jobtracker/jobtrackerapi/service/JobTrackerService.java
+++ src/main/java/com/jobtracker/jobtrackerapi/service/JobTrackerService.java
@@ -1,4 +1,17 @@
 package com.jobtracker.jobtrackerapi.service;
 
+import com.jobtracker.jobtrackerapi.entity.JobApplication;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface JobTrackerService {
+
+    JobApplication addJobApplication(JobApplication jobApplication);
+
+    Optional<JobApplication> getJobApplicationById(Integer jobApplicationId);
+
+    void deleteJobApplication(Integer jobApplicationId);
+
+    List<JobApplication> getAllJobApplications();
 }
